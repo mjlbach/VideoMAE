daa0ae24a799353e79c6b9ac8099bb3916a211c9
Michael Lingelbach <m.j.lbach@gmail.com>
2022-08-18 08:02:27 -0700

    Update

--- batch_scripts/data_generation/generate_label.py
+++ batch_scripts/data_generation/generate_label.py
@@ -1,18 +1,20 @@
 import os
 import argparse
 import numpy as np
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_folder", required=True)
 parser.add_argument("--split", nargs='+', help='Train-test-val split', type=float, default=[0.8, 0.1, 0.1])
 args = parser.parse_args()
-
-def absolute_file_paths(directory):
-    path = os.path.abspath(directory)
-    return [entry.path for entry in os.scandir(path) if entry.is_file()]
+#
+# def absolute_file_paths(directory):
+#     breakpoint()
+#     path = os.path.abspath(directory)
+#     return [entry.path for entry in os.scandir(path) if entry.is_file()]
 
 # "/vision/group/ego4d/v1/full_scale"
-mp4_list = absolute_file_paths(args.video_folder)
+mp4_list = list(Path(args.video_folder).rglob("*.mp4"))
 labels = ["" + str(i) for i in range(len(mp4_list))]
 
 assert np.sum(args.split) == 1
--- batch_scripts/pretrain/experiments_jc/single_mask_10.sh
+++ batch_scripts/pretrain/experiments_jc/single_mask_10.sh
@@ -1,12 +1,12 @@
 #!/bin/bash           
 
 #SBATCH --job-name=mvae_ssv2_single_node_10
-#SBATCH --partition=viscam
+#SBATCH --partition=svl
 #SBATCH --time=128:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:a5000:1
+#SBATCH --gres=gpu:titanrtx:1
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=m.j.lbach@gmail.com
 
@@ -18,20 +18,20 @@ cd ~/Repositories/VideoMAE
 OUTPUT_DIR='/viscam/u/mjlbach/video_memory_project/ssv2_10'
 DATA_PATH='/viscam/data/SomethingSomethingV2/20bn-something-something-v2_sta_web_140w_320p/train.csv'
 
-head_node_ip=$(hostname --ip-address)
+export ip_node_0=$(hostname --ip-address)
 
-python3 -m torch.distributed.launch --nproc_per_node=1 \
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=1 \
         --master_port 12320 --nnodes=1 \
-        --node_rank=0 --master_addr="$head_node_ip" \
+        --node_rank=0 --master_addr=$ip_node_0 \
         run_mae_pretraining.py \
         --data_path ${DATA_PATH} \
         --mask_type tube \
         --mask_ratio 0.1 \
         --model pretrain_videomae_base_patch16_224 \
         --decoder_depth 4 \
-        --batch_size 28 \
+        --batch_size 4 \
         --num_frames 16 \
-        --sampling_rate 4 \
+        --sampling_rate 2 \
         --opt adamw \
         --opt_betas 0.9 0.95 \
         --warmup_epochs 40 \
