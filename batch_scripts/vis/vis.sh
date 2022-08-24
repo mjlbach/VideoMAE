c1bc9eae62075b37764e282214696f2da1b4b3f8
Michael Lingelbach <m.j.lbach@gmail.com>
2022-08-24 13:48:53 -0700

    Update run scripts

--- batch_scripts/vis/vis.sh
+++ batch_scripts/vis/vis.sh
@@ -1,14 +1,13 @@
 # Set the path to save video
-OUTPUT_DIR='VideoMAE/demo/test'
+OUTPUT_DIR='TODO/VideoMAE/demo/vis_k400_1_0.9'
 # path to video for visualization
-VIDEO_PATH='/vision/group/ego4d/v1/full_scale/000786a7-3f9d-4fe6-bfb3-045b368f7d44.mp4'
+VIDEO_PATH='TODO/TODO.mp4'
 # path to pretrain model
-MODEL_PATH='/svl/u/mjlbach/checkpoint.pth'
+MODEL_PATH='TODO/videomae_pretrain_base_patch16_224_frame_16x4_tube_mask_ratio_0.9_e1600/checkpoint-1599.pth'
 
 python3 run_videomae_vis.py \
     --mask_ratio 0.9 \
     --mask_type tube \
     --decoder_depth 4 \
-    --sampling_rate 4 \
     --model pretrain_videomae_base_patch16_224 \
     ${VIDEO_PATH} ${OUTPUT_DIR} ${MODEL_PATH}
