b8df7283f1c5b024e87ae4c34430a1837391d0bf
Michael Lingelbach <m.j.lbach@gmail.com>
2022-09-19 15:04:31 -0700

    Disable nccl, aggregate gradients

--- batch_scripts/pretrain/ssv2_mask_90.sh
+++ batch_scripts/pretrain/ssv2_mask_90.sh
@@ -1,25 +1,26 @@
 #!/bin/bash           
 
-#SBATCH --job-name=mvae_ssv2_single_node_90
-#SBATCH --partition=macondo
+#SBATCH --job-name=mvae_ssv2_8
+#SBATCH --partition=viscam
 #SBATCH --time=124:00:00
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:titanrtx:4
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=5G
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:a40:8
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=m.j.lbach@gmail.com
 
 export MASTER_PORT=$((12000 + $RANDOM % 20000))
 export OMP_NUM_THREADS=1
+export NCCL_P2P_LEVEL=NVL
 
 cd ~/Repositories/VideoMAE
 
 source ~/.bashrc  
 conda activate mvae
 
-OUTPUT_DIR='/viscam/u/mjlbach/video_memory_project/ssv2_90'
+OUTPUT_DIR='/viscam/u/mjlbach/video_memory_project/ssv2_90_accum'
 DATA_PATH='/viscam/data/SomethingSomethingV2/20bn-something-something-v2_sta_web_140w_320p/train.csv'
 
 
@@ -31,6 +32,7 @@ srun python run_mae_pretraining.py \
         --decoder_depth 4 \
         --batch_size 36 \
         --num_frames 16 \
+        --accum_iter 8 \
         --sampling_rate 2 \
         --opt adamw \
         --opt_betas 0.9 0.95 \
